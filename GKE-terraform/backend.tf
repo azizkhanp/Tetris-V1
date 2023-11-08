0c3cfb1f9455b58609e13ba988b4d15d1ff73499
pathan <azizkhan.cool@gmail.com>
2023-11-08 12:30:34 +0530

    Update backend.tf

--- GKE-terraform/backend.tf
+++ GKE-terraform/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "gcs" {
     bucket  = "aziz-terraform-pro" # replace with your GCS bucket name
     credentials = "devops-403405-ea5d8b6254a3.json"
-    project = "devops-403405"
-    region = "us-west4" 
+    
   }
 }
