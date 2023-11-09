bfefaabededa40b0d7253ba7282f4cdffc7b5c47
Aziz <azizkhan.cool@gmail.com>
2023-11-09 12:53:11 +0530

    commit

--- GKE-terraform/main.tf
+++ GKE-terraform/main.tf
@@ -1,22 +1,22 @@
+resource "google_service_account" "gke_service_account" {
+  account_id   = "gke-SA"
+  display_name = "GKE Service Account"
+}
+
+resource "google_project_iam_member" "gke_cluster_role" {
+  project = "devops-403405"
+  role    = "roles/container.admin"
+  member  = "serviceAccount:${google_service_account.gke_service_account.email}"
+}
+
+
 # Create a GKE cluster
 resource "google_container_cluster" "gke-cluster" {
   name     = "gke-cluster"
   location = "us-east1"
   deletion_protection ="false"
-  initial_node_count = 1
+  initial_node_count = 2
   remove_default_node_pool = true
-}
-
-# Create a service account for the GKE nodes
-resource "google_service_account" "node_service_account" {
-  account_id = "node-sa"
-}
-
-# Grant necessary permissions to the service account
-resource "google_project_iam_member" "service_account_permissions" {
-  project = "devops-403405"
-  role    = "roles/container.admin" # Replace with the necessary role
-  member  = "serviceAccount:node-sa@devops-403405.iam.gserviceaccount.com"
 }
 
 # Create a GKE node pool
@@ -24,10 +24,10 @@ resource "google_container_node_pool" "node_pool" {
   name       = "node-pool"
   location   = google_container_cluster.gke-cluster.location
   cluster    = google_container_cluster.gke-cluster.name
-  node_count = 1
+  initial_node_count = 2
 
   node_config {
-    service_account = google_service_account.node_service_account.email
+    service_account = google_service_account.gke_service_account.email
     disk_size_gb = "10"
     machine_type = "e2-small" # Replace with your desired machine type
     # Additional node configurations
