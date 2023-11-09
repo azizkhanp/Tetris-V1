cd30e471a8c4bf17eb0496b3d4e0530e82e5b86d
Aziz <azizkhan.cool@gmail.com>
2023-11-09 14:40:51 +0530

    commit

--- GKE-terraform/1.backend.tf
+++ GKE-terraform/1.backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket  = "aziz-terraform-pro" # replace with your GCS bucket name
-    credentials = "devops-403405-ea5d8b6254a3.json"
+    prefix = "terraform/state"
 
   }
 }
